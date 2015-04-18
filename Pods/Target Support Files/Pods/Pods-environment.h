b292444322a675b9b52bdc6438f1f1abd1c6c9eb
RainbowMango <qdurenhongcai@163.com>
2015-04-18 23:20:37 +0800

    integrate AGImagePickerController

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AGImagePickerController
+#define COCOAPODS_POD_AVAILABLE_AGImagePickerController
+#define COCOAPODS_VERSION_MAJOR_AGImagePickerController 2
+#define COCOAPODS_VERSION_MINOR_AGImagePickerController 0
+#define COCOAPODS_VERSION_PATCH_AGImagePickerController 1
+
